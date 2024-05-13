ec9f5d905b3204ee7c881c3c52abb163590b4373
jinwoo <zvzv9808@gmail.com>
2024-05-13 18:02:42 +0900

    refactor: add member info in JWT Token Dto

--- src/main/java/com/ssafy/happyhouse/global/token/JwtTokenDto.java
+++ src/main/java/com/ssafy/happyhouse/global/token/JwtTokenDto.java
@@ -1,6 +1,7 @@
 package com.ssafy.happyhouse.global.token;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ssafy.happyhouse.entity.member.constant.Role;
 import lombok.*;
 
 import java.util.Date;
@@ -13,6 +14,8 @@ import java.util.Date;
 public class JwtTokenDto {
 
     private Long id;
+    private String username;
+    private Role role;
     private String grantType;
     private String accessToken;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
@@ -20,4 +23,18 @@ public class JwtTokenDto {
     private String refreshToken;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Date refreshTokenExpireTime;
+
+    @Override
+    public String toString() {
+        return "JwtTokenDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", role=" + role +
+                ", grantType='" + grantType + '\'' +
+                ", accessToken='" + accessToken + '\'' +
+                ", accessTokenExpireTime=" + accessTokenExpireTime +
+                ", refreshToken='" + refreshToken + '\'' +
+                ", refreshTokenExpireTime=" + refreshTokenExpireTime +
+                '}';
+    }
 }
