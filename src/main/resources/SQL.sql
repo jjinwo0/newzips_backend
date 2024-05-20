004a08019bba1c6f11b8aacfda519ae399ca3b57
JuneYub <ajsjdlwj0123@gmail.com>
2024-05-20 15:36:34 +0900

    docs : sql 정리

--- src/main/resources/SQL.sql
+++ src/main/resources/SQL.sql
@@ -32,3 +32,44 @@ WHERE mainCategoryCode NOT IN ('P1', 'Q1', 'I2', 'R1')
 
 -- 아파트명 인덱스 생성
 ALTER TABLE houseinfo ADD FULLTEXT INDEX houseinfo_idx(apartmentName) WITH PARSER ngram;
+
+-- 뉴스 테이블
+CREATE TABLE 'news' (
+	'id' int NOT NULL AUTO_INCREAMENT,
+    'newsDate' date DEFAULT NULL,
+    'newsTitle' varchar(100) DEFAULT NULL,
+    'newsLink' varchar(500) DEFAULT NULL,
+    PRIMARY KEY('id')
+    ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+use ssafyhome;
+show tables;
+
+drop table if exists members;
+
+-- 멤버 테이블
+create table members (
+    member_id bigint primary key auto_increment,
+    username varchar(200) not null,
+    email varchar(300) not null,
+    password varchar(100) not null default '1234',
+    role varchar(10) not null,
+    member_type varchar(20) not null,
+    nickname varchar(100),
+    profile varchar(250) not null,
+    refresh_token varchar(500),
+    refresh_token_expiration_time datetime(3)
+);
+
+insert into members(username, email, password, role, member_type, nickname, profile) values('testAdmin', 'admin@happyhouse.com', '1234', 'ADMIN', 'LOCAL', '관리자', 'https://api.dicebear.com/8.x/pixel-art/svg?seed=testAdmin');
+
+-- 공지사항 테이블
+CREATE TABLE board (
+    id bigint AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    author VARCHAR(100),
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
