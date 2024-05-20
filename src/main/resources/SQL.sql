bee74ca4fd90a05cff987846b6c4cbbb1871277a
jinwoo <zvzv9808@gmail.com>
2024-05-20 15:38:56 +0900

    docs: SQL 쿼리문 추가 작성

--- src/main/resources/SQL.sql
+++ src/main/resources/SQL.sql
@@ -47,21 +47,62 @@ show tables;
 
 drop table if exists members;
 
--- 멤버 테이블
+-- 회원 테이블
 create table members (
-    member_id bigint primary key auto_increment,
-    username varchar(200) not null,
-    email varchar(300) not null,
-    password varchar(100) not null default '1234',
-    role varchar(10) not null,
-    member_type varchar(20) not null,
-    nickname varchar(100),
-    profile varchar(250) not null,
-    refresh_token varchar(500),
-    refresh_token_expiration_time datetime(3)
+                         member_id bigint primary key auto_increment,
+                         username varchar(200) not null,
+                         email varchar(300) not null,
+                         password varchar(100) not null default '1234',
+                         role varchar(10) not null,
+                         member_type varchar(20) not null,
+                         nickname varchar(100),
+                         profile varchar(250) not null,
+                         price int,
+                         refresh_token varchar(500),
+                         refresh_token_expiration_time datetime(3)
 );
 
-insert into members(username, email, password, role, member_type, nickname, profile) values('testAdmin', 'admin@happyhouse.com', '1234', 'ADMIN', 'LOCAL', '관리자', 'https://api.dicebear.com/8.x/pixel-art/svg?seed=testAdmin');
+insert into members(username, email, password, role, member_type, nickname, profile, price) values('testAdmin', 'admin@happyhouse.com', '1234', 'ADMIN', 'LOCAL', '관리자', 'https://api.dicebear.com/8.x/pixel-art/svg?seed=testAdmin', null);
+insert into members(username, email, password, role, member_type, nickname, profile, price) values('testExpert', 'admin@happyhouse.com', '1234', 'EXPERT', 'LOCAL', '박프로', 'https://api.dicebear.com/8.x/pixel-art/svg?seed=testExpert', 10000);
+insert into members(username, email, password, role, member_type, nickname, profile, price) values('testExpert2', 'admin@happyhouse.com', '1234', 'EXPERT', 'LOCAL', '김프로', 'https://api.dicebear.com/8.x/pixel-art/svg?seed=testExpert2', 40000);
+insert into members(username, email, password, role, member_type, nickname, profile, price) values('testExpert3', 'admin@happyhouse.com', '1234', 'EXPERT', 'LOCAL', '최프로', 'https://api.dicebear.com/8.x/pixel-art/svg?seed=testExpert3', 70000);
+insert into members(username, email, password, role, member_type, nickname, profile, price) values('testExpert4', 'admin@happyhouse.com', '1234', 'EXPERT', 'LOCAL', '황프로', 'https://api.dicebear.com/8.x/pixel-art/svg?seed=testExpert4', 90000);
+
+update members set price=100 where member_id=5;
+
+select * from members;
+
+drop table if exists room;
+
+-- 채팅방 테이블
+create table room (
+                      room_id bigint primary key auto_increment,
+                      status varchar(20),
+                      entired_time datetime(3),
+                      expert_id bigint,
+                      member_id bigint,
+                      foreign key (expert_id) references members(member_id),
+                      foreign key (member_id) references members(member_id)
+);
+
+select * from room;
+
+insert into room(status, entired_time, expert_id, member_id) values('LIVE', DATE_ADD(NOW(), INTERVAL 60 MINUTE), 2, 1);
+insert into room(status, entired_time, expert_id, member_id) values('LIVE', DATE_ADD(NOW(), INTERVAL 60 MINUTE));
+insert into room(status, entired_time, expert_id, member_id) values('LIVE', DATE_ADD(NOW(), INTERVAL 60 MINUTE));
+insert into room(status, entired_time, expert_id, member_id) values('LIVE', DATE_ADD(NOW(), INTERVAL 60 MINUTE));
+insert into room(status, entired_time, expert_id, member_id) values('LIVE', DATE_ADD(NOW(), INTERVAL 60 MINUTE));
+
+drop event update_status_if_expired;
+
+-- 채팅방 Status 업데이트 이벤트
+CREATE EVENT update_status_if_expired
+ON SCHEDULE EVERY 1 MINUTE
+DO
+UPDATE SSAFYHOME.ROOM
+SET status = 'EXPIRED'
+WHERE entired_time < NOW();
+
 
 -- 공지사항 테이블
 CREATE TABLE board (
